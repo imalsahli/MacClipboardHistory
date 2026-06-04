abdfbb509d47eba616dcaae808d23db0f3bb9634
Mohammed Alsahli <medooni@gmail.com>
2026-06-04 16:14:51 +0300

    Update code

--- src/main/java/org/example/ClipboardHistory.java
+++ src/main/java/org/example/ClipboardHistory.java
@@ -5,15 +5,21 @@ public class ClipboardHistory {
 
     private ArrayList<ClipboardItem> items = new ArrayList<>();
 
+    // addText(text)
+ 
     public void addText(String text){
         ClipboardItem item = new ClipboardItem(text);
         items.add(item);
     }
 
-
+    // getAllItems()   
     public ArrayList<ClipboardItem> getAllItems(){
         return items;
 
+    }
+
+    public void clearAll(){
+        items.clear();
     }
 
 }
--- src/main/java/org/example/ClipboardService.java
+++ src/main/java/org/example/ClipboardService.java
@@ -6,8 +6,29 @@ import java.awt.datatransfer.Transferable;
 
 
 public class ClipboardService {
+    /*
+    1. Get system clipboard
+    2. Get clipboard content
+    3. Check if content is text
+    4. Return text
+    5. If not text, return empty string
+    */ 
+   public String getCurrentText() {
+        try {
+            // Get the system clipboard
+            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
 
-    public String getCurrentText() {
+            // Get the clipboard content
+            Transferable content = clipboard.getContents(null);
+
+            // Check if the content is text
+            if (content != null && content.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+                // Return the text from the clipboard
+                return (String) content.getTransferData(DataFlavor.stringFlavor);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return "";
     }
 }
